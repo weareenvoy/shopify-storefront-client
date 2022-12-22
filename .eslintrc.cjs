58d50d060f9f0e6d032cab14bf8e512a3920311e
Socrates Nguyen <socrates@weareenvoy.com>
2022-12-22 12:42:30 -0800

    add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,9 +3,9 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: 'airbnb-base',
-  overrides: [
-  ],
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
+  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
@@ -14,5 +14,6 @@ module.exports = {
     'import/prefer-default-export': 'off',
     'no-console': 'off',
     'no-unused-vars': 'warn',
+    'prettier/prettier': 'warn',
   },
 };
